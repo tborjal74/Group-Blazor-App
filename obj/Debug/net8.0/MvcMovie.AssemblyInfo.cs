b54d0d402d77f92652480caf5f7ac60e4d79f97c
Adam Lind <adamsplind@gmail.com>
2025-11-20 18:51:32 -0700

    rebuild app

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42c6598248fd95d214ba5300f11fabcc8ef4e354")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4df19bac0ee4542c3b829f756c292122172eb4c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
